04963fdfb2ec82608eb2c44d079ddb1083783e04
vidhi98 <41187088+vidhi98@users.noreply.github.com>
2020-09-27 17:01:58 +0530

    Update settings.py

--- doc/doc/settings.py
+++ doc/doc/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'k*-9@8ecnhc(3jrmyv%%x=_9vwhs-rgjr!+6$zo-e$yfuyshy9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
