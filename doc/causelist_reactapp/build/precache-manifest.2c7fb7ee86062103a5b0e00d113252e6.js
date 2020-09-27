9d011f942065538f8c3c51437c42f4254ec21792
Vidhi <vidhipandey998@gmail.com>
2020-09-27 16:33:52 +0530

    build folder added

---
+++ doc/causelist_reactapp/build/precache-manifest.2c7fb7ee86062103a5b0e00d113252e6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbb4e5b9f244ddf70ea82674adaf6449",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4b7fdac1e8c77f0b51b",
+    "url": "/static/css/2.847e01e0.chunk.css"
+  },
+  {
+    "revision": "3014c50a7219fcc7fcc3",
+    "url": "/static/css/main.643e2f31.chunk.css"
+  },
+  {
+    "revision": "a4b7fdac1e8c77f0b51b",
+    "url": "/static/js/2.a2d60faf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.a2d60faf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3014c50a7219fcc7fcc3",
+    "url": "/static/js/main.062f5ec8.chunk.js"
+  },
+  {
+    "revision": "988bdb5add3c6364503d",
+    "url": "/static/js/runtime-main.05594ae8.js"
+  },
+  {
+    "revision": "9222e1d902ca874c4d2443e2a8e881c9",
+    "url": "/static/media/Group 12.9222e1d9.svg"
+  },
+  {
+    "revision": "e57cf6ad78278311b9d0cc6f51b38586",
+    "url": "/static/media/Group 15.e57cf6ad.svg"
+  },
+  {
+    "revision": "ad4f95c3d8ccff8ace947c7f24a6bb77",
+    "url": "/static/media/Group 1861.ad4f95c3.svg"
+  },
+  {
+    "revision": "fa8eb987cc839f233337c14003161414",
+    "url": "/static/media/croquet.fa8eb987.svg"
+  },
+  {
+    "revision": "52dfb35f82fe3b1bbda8c7e5a8bfbaa5",
+    "url": "/static/media/filter.52dfb35f.svg"
+  },
+  {
+    "revision": "4056290a8630399bac905eba525bbaab",
+    "url": "/static/media/magazine.4056290a.svg"
+  },
+  {
+    "revision": "ac1ac386901f0b2a851d746b7de26976",
+    "url": "/static/media/pages.ac1ac386.svg"
+  }
+]);
--- doc/doc/settings.py
+++ doc/doc/settings.py
@@ -83,6 +83,8 @@ DATABASES = {
 }
 
 
+ADMINS = (('vidhi','docker@docker'))
+
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
